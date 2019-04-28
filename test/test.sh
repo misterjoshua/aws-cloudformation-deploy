edcde00eb9e652937412a5a15c028f52adcf7cc9
Josh Kellendonk <josh@colloco.com>
2019-04-28 15:07:03 -0600

    Add status check from cloudformation

--- test/test.sh
+++ test/test.sh
@@ -7,19 +7,29 @@ touch /test/ready
 
 # Wait for deployment to finish.
 WHEN_DONE=/test/done
-for i in $(seq 12); do
+for i in $(seq 120); do
     if [ -f "$WHEN_DONE" ]; then
         break;
     fi
 
     echo "Waiting for the deployment to finish."
-    sleep 5
+    sleep 1
 done
 
 # Timeout
 if [ ! -f "$WHEN_DONE" ]; then
     echo "Timed out"
     exit 1
+fi
+
+STACK_NAME=$(cat $WHEN_DONE)
+
+aws cloudformation describe-stack-events --stack-name "$STACK_NAME" >status
+STATUS=$(jq <status -r '.StackEvents[0].ResourceStatus')
+
+echo "Status of deploy is $STATUS"
+if [ ! "$STATUS" = "UPDATE_COMPLETE" ]; then
+    echo "Deploy did not succeed."
 fi
 
 echo "It finished"
